e9e2e34ad509f7ff567c2e75651e123954091df4
PC App Developer <seksky333@gmail.com>
2024-02-24 21:20:52 +1100

    update ef setup for Core Identity to accommodate MySql settings

--- AccountAPI/Data/ApplicationDbContext.cs
+++ AccountAPI/Data/ApplicationDbContext.cs
@@ -9,4 +9,25 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {       
+        base.OnModelCreating(builder);
+        builder.Entity<IdentityUser>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+        
+        builder.Entity<IdentityUser>(entity => entity.Property(m => m.NormalizedUserName).HasMaxLength(85));
+        
+        builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+        builder.Entity<IdentityRole>(entity => entity.Property(m => m.NormalizedName).HasMaxLength(85));
+        builder.Entity<IdentityRole>(entity => entity.Property(m => m.NormalizedName).HasMaxLength(85));
+        
+        builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+        builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+        
+        builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.LoginProvider).HasMaxLength(85));
+        builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.ProviderKey).HasMaxLength(85));
+        
+        builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.LoginProvider).HasMaxLength(85));
+        builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.Name).HasMaxLength(85));
+    }
 }
